441e3632a0782bc639f1d286ea6b8fa8529f6794
Kristian Fulkerson <kristianf451@gmail.com>
2022-01-15 20:58:54 -0800

    good

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -1,25 +1,6 @@
 const db = require("../../data/db-config");
 
 const getRecipeById = async (recipe_id) => {
-  // const recipe = await db("recipes as r")
-  //   .leftJoin("steps as s", "r.recipe_id", "s.recipe_id")
-  //   .leftJoin("step_ingredients as si", "s.step_id", "si.step_id")
-  //   .leftJoin("ingredients as i", "si.ingredient_id", "i.ingredient_id")
-  //   .select(
-  //     "r.recipe_id",
-  //     "r.recipe_name",
-  //     "s.step_id",
-  //     "s.step_number",
-  //     "s.step_instructions",
-  //     "si.ingredient_id",
-  //     "i.ingredient_name",
-  //     "si.quantity",
-  //     "i.ingredient_unit"
-  //   )
-  //   .where("r.recipe_id", recipe_id)
-  //   .orderBy("s.step_number", "asc")
-
-  //step and recipe info
   const recipeWithSteps = await db("recipes as r")
     .select("r.*", "s.step_id", "s.step_number", "s.step_instructions")
     .leftJoin("steps as s", "r.recipe_id", "s.recipe_id")
